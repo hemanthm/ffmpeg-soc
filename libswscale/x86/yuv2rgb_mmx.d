9204a660a5603958bac6424334138355229a5327
S.N. Hemanth Meenakshisundaram <hemanth@localhost.localdomain>
2010-07-10 19:27:22 -0700

    Deleting object files wrongly added earlier

--- libswscale/x86/yuv2rgb_mmx.d
+++
@@ -1,9 +0,0 @@
-libswscale/x86/yuv2rgb_mmx.o: libswscale/x86/yuv2rgb_mmx.c config.h \
- libswscale/rgb2rgb.h libswscale/swscale.h libavutil/avutil.h \
- libavutil/common.h libavutil/attributes.h libavutil/intmath.h \
- libavutil/x86/intmath.h libavutil/mem.h libavutil/internal.h \
- libavutil/timer.h libavutil/x86/timer.h libavutil/libm.h \
- libavutil/error.h libavutil/avutil.h libavutil/mathematics.h \
- libavutil/rational.h libavutil/intfloat_readwrite.h libavutil/log.h \
- libavutil/pixfmt.h libavutil/avconfig.h libswscale/swscale_internal.h \
- libavutil/x86_cpu.h libswscale/x86/yuv2rgb_template.c

73dba82b8c47636433a8f760e37f496a606ab154
hemanthm <smeenaks@ucsd.edu>
2010-08-25 00:21:52 -0700

    Fix linesize and data pointers for packed data in AVFilterBufferRef

--- libavfilter/defaults.c
+++ libavfilter/defaults.c
@@ -117,10 +117,10 @@ AVFilterBufferRef *avfilter_default_get_audio_buffer(AVFilterLink *link, int per
 
     /* Set the number of bytes to traverse to reach next sample of a particular channel:
      * For planar, this is simply the sample size.
-     * For packed, this is the number of samples * sample_size.
+     * For packed, this is the number of channels * sample_size.
      */
     for (i = 0; i < chans_nb; i++)
-        samples->linesize[i] = planar > 0 ? per_channel_size : sample_size;
+        samples->linesize[i] = planar > 0 ? sample_size : (chans_nb*sample_size);
     memset(&samples->linesize[chans_nb], 0, (8-chans_nb) * sizeof(samples->linesize[0]));
 
     /* Calculate total buffer size, round to multiple of 16 to be SIMD friendly */
@@ -139,8 +139,10 @@ AVFilterBufferRef *avfilter_default_get_audio_buffer(AVFilterLink *link, int per
             samples->data[i] = buf + step_size;
         }
     } else {
-        for (i = 1; i < chans_nb; i++)
-            samples->data[i] = buf;
+        for (i = 1; i < chans_nb; i++) {
+            step_size += sample_size;
+            samples->data[i] = buf + step_size;
+        }
     }
 
     memset(&samples->data[chans_nb], 0, (8-chans_nb) * sizeof(samples->data[0]));
